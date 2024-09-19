481284009276ee56e01467590f766dc3987c4d2a
Danil <danil.shapoval21@gmail.com>
2024-09-19 15:54:59 +0100

    Обновление .gitignore и lib/mongodb.ts, добавление .env.example

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -1,8 +1,8 @@
 import mongoose from 'mongoose';
 
-const MONGODB_URI = process.env.MONGODB_URI;
+const uri = process.env.MONGODB_URI;
 
-if (!MONGODB_URI) {
+if (!uri) {
   throw new Error('Please define the MONGODB_URI environment variable inside .env.local');
 }
 
@@ -22,7 +22,7 @@ export async function connectToDatabase() {
       bufferCommands: false,
     };
 
-    cached.promise = mongoose.connect(MONGODB_URI!, opts).then((mongoose) => {
+    cached.promise = mongoose.connect(uri!, opts).then((mongoose) => {
       return mongoose;
     });
   }
